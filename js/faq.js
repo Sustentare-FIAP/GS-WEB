a2d7c9748290dc0b4e81d10d5540bb76bbd7caac
dogzeira <douglasdanilobutu13@gmail.com>
2024-11-18 22:18:46 -0300

    Criação página sobre nos]

--- js/faq.js
+++ js/faq.js
@@ -1,74 +1,71 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const faqData = [
-      {
-        question: "Quais são as formas de pagamento aceitas no seguro bike?",
-        answer:
-          "Até 6x sem juros por débito automático em conta corrente ou cartão de crédito de qualquer bandeira; Até 12x no Cartão de Crédito Porto Bank, com desconto especial de 5%",
-      },
-      {
-        question: "A cobertura de roubo garante furto simples?",
-        answer:
-          "A cobertura de roubo garante a subtração da bicicleta e dos acessórios, mediante o emprego de violência ou o rompimento de obstáculos do local, desde que existam vestígios materiais. Não é o caso de amparos para simples desaparecimentos ou extravios.",
-      },
-      {
-        question: "Qual é o valor da franquia do Seguro Bike da Porto?",
-        answer:
-          "O valor da franquia, ou seja, participação do segurado na hora de pagar pela reparação de um dano, pode variar de acordo com o preço da sua bike e essa informação está detalhada no documento do seguro (apólice). Este valor é o mesmo que foi definido junto ao corretor no momento da negociação do seguro e só ocorre quando há indenização por parte da seguradora.",
-      },
-      {
-        question: "O seguro bike tem cobertura para terceiros?",
-        answer:
-          "Sim, de forma opcional. Chamamos de cobertura de Responsabilidade Civil, que oferece amparo para danos materiais ou corporais causados a terceiros durante o uso da bicicleta. Ex.: a bike atingiu uma pessoa ou veículo de alguém, a cobertura ampara essas situações, desde que a pessoa atingida não seja alguém da sua família.",
-      },
-      {
-        question:
-          "Qual é o valor mínimo da bicicleta para que eu possa contratar o Seguro Bike da Porto?",
-        answer:
-          "Você pode contratar o seguro para bicicletas com valores a partir de R$ 2.000,00",
-      },
-      {
-        question: "Posso contratar o seguro de bike para bicicleta usada?",
-        answer: `
-          <p>Você pode contratar o seguro para: </p>
+  const faqData = [
+    {
+      question: "Como funciona o programa de benefícios?",
+      answer:
+        "Nosso programa recompensa ações sustentáveis! Quando você realiza práticas ecológicas, como instalar energia solar, reduzir o consumo de eletricidade ou participar de eventos educativos, acumula pontos que podem ser trocados por descontos, produtos sustentáveis ou doações para causas ambientais.",
+    },
+    {
+      question: "Quem pode participar do programa?",
+      answer:
+        "O programa é aberto para qualquer pessoa interessada em promover a sustentabilidade e adotar práticas que contribuam para a preservação do meio ambiente. Empresas também podem participar como parceiras, oferecendo recompensas e ampliando sua visibilidade no mercado sustentável.",
+    },
+    {
+      question: "Quais tipos de recompensas estão disponíveis?",
+      answer:
+        "As recompensas incluem descontos em produtos e serviços de parceiros, itens ecológicos como garrafas reutilizáveis e sacolas sustentáveis, consultorias especializadas em energia limpa, e até mesmo a conversão de pontos em doações para projetos ambientais.",
+    },
+    {
+      question: "Como as empresas podem se tornar parceiras?",
+      answer:
+        "Empresas interessadas em ser parceiras podem entrar em contato conosco para cadastrar suas ofertas no programa. Como parceiras, elas ganham visibilidade na plataforma, acesso a consumidores conscientes e recebem o Selo de Empresa Sustentável para exibir em seus canais digitais.",
+    },
+    {
+      question: "Como posso acumular pontos?",
+      answer: `
+          <p>Você pode acumular pontos de várias formas:</p>
           <ul class="body-postion-faq">
-            <li>Bicicleta tradicional nova;</li>
-            <li>Bicicleta tradicional com até 8 anos de uso;</li>
-            <li>Bicicleta elétrica com até 3 anos de uso;</li>
+            <li>Realizando ações sustentáveis, como instalação de painéis solares ou compra de aparelhos eficientes.</li>
+            <li>Participando de workshops e eventos sobre energia renovável.</li>
+            <li>Divulgando práticas ecológicas nas redes sociais e indicando amigos para o programa.</li>
           </ul>
         `,
-      },
-    ];
-  
-    const faqContainer = document.getElementById("faq-container");
-  
-    faqData.forEach((faq, index) => {
-      const faqItem = document.createElement("div");
-      faqItem.className = `faq-${index}`;
-  
-      const question = document.createElement("h2");
-      question.className = "faq-page";
-      question.textContent = faq.question;
-      question.addEventListener("click", () => toggleAccordion(index));
-  
-      const answer = document.createElement("div");
-      answer.className = "faq-body";
-      answer.innerHTML = faq.answer;
-      answer.style.display = "none";
-  
-      faqItem.appendChild(question);
-      faqItem.appendChild(answer);
-      faqContainer.appendChild(faqItem);
-  
-      const separator = document.createElement("hr");
-      faqItem.appendChild(separator);
-    });
-  
-    function toggleAccordion(index) {
-      const faqItems = faqContainer.querySelectorAll(".faq-body");
-      const questionItems = faqContainer.querySelectorAll(".faq-page");
-      faqItems[index].style.display =
-        faqItems[index].style.display === "block" ? "none" : "block";
-      questionItems[index].classList.toggle("active");
-    }
+    },
+    {
+      question: "O que é o Selo de Empresa Sustentável?",
+      answer: "O Selo de Empresa Sustentável é um símbolo exclusivo concedido às empresas parceiras do programa. Ele certifica que a empresa está engajada em práticas e iniciativas voltadas para a sustentabilidade. O selo pode ser exibido no footer do site da empresa, reforçando seu compromisso com o meio ambiente e destacando-a como líder no mercado sustentável."
+    },
+  ];
+
+  const faqContainer = document.getElementById("faq-container");
+
+  faqData.forEach((faq, index) => {
+    const faqItem = document.createElement("div");
+    faqItem.className = `faq-${index}`;
+
+    const question = document.createElement("h2");
+    question.className = "faq-page";
+    question.textContent = faq.question;
+    question.addEventListener("click", () => toggleAccordion(index));
+
+    const answer = document.createElement("div");
+    answer.className = "faq-body";
+    answer.innerHTML = faq.answer;
+    answer.style.display = "none";
+
+    faqItem.appendChild(question);
+    faqItem.appendChild(answer);
+    faqContainer.appendChild(faqItem);
+
+    const separator = document.createElement("hr");
+    faqItem.appendChild(separator);
   });
-  
+
+  function toggleAccordion(index) {
+    const faqItems = faqContainer.querySelectorAll(".faq-body");
+    const questionItems = faqContainer.querySelectorAll(".faq-page");
+    faqItems[index].style.display =
+      faqItems[index].style.display === "block" ? "none" : "block";
+    questionItems[index].classList.toggle("active");
+  }
+});
